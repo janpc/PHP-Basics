e532f1b5f951979335021ff81c1a7d0b038a6895
Jan Puig <janpuigcapella@gmail.com>
2021-01-28 16:30:51 +0100

    done operators

---
+++ operators.php
@@ -0,0 +1,26 @@
+<?php
+echo " -- 1+2 " .  var_dump(1+2) . "<br>";
+echo " -- 1-2 " . var_dump(1-2) . "<br>";
+echo " -- 1*2 " . var_dump(1*2) . "<br>";
+echo " -- 1/2 " . var_dump(1/2) . "<br>";
+echo " -- 1%2 " . var_dump(1%2) . "<br>";
+
+echo "<br><br><br>";
+
+echo " -- 1==2 " . var_dump(1==2) . "<br>";
+echo " -- 1!=2 " . var_dump(1!=2) . "<br>";
+echo " -- 1<2 " . var_dump(1<2) . "<br>";
+echo " -- 1>2 " . var_dump(1>2) . "<br>";
+echo " -- 1<=2 " . var_dump(1<=2) . "<br>";
+echo " -- 1>=2 " . var_dump(1>=2) . "<br>";
+
+echo "<br><br><br>";
+
+echo " -- 1==1 && 1==2 " . var_dump(1==1 && 1==2) . "<br>";
+echo " -- 1==1 And 1==2 " . var_dump(1==1 And 1==2) . "<br>";
+echo " -- 1==1 || 1==2 " . var_dump(1==1 || 1==2) . "<br>";
+echo " -- !1==1 " . var_dump(!1==1) . "<br>";
+echo " -- 1==1 Xor 1==2 " . var_dump(1==1 Xor 1==2) . "<br>";
+echo " -- 1==5 Xor 1==2 " . var_dump(1==5 Xor 1==2) . "<br>";
+echo " -- 1==1 Xor 2==2 " . var_dump(1==1 Xor 2==2) . "<br>";
+?>
